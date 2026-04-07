08e4ddb5a0a8a0fb0f41c93f281debf1c877756f
Reesol <harrysolomons@gmail.com>
2026-04-07 14:57:16 +0100

    Switched to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Home } from './pages/Home'
 import { Flappy } from './pages/Flappy'
@@ -8,16 +8,16 @@ import { Ropemakers } from './pages/Ropemakers'
 function App() {
 
   return (
-    <BrowserRouter basename='/iasiap'>
       <section id="center">
+        <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/XXXXXX" element={<NiceTry />} />
           <Route path="/bookcode" element={<Ropemakers />} />
           <Route path="/boatname" element={<Flappy />} />
-      </Routes>
+        </Routes>
+      </HashRouter>
       </section>
-    </BrowserRouter>
   )
 }
 
