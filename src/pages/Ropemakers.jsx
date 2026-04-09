a7f35572072e7dfbb4cb4f3cdbce6d840c0e0fc4
Reesol <harrysolomons@gmail.com>
2026-04-09 15:52:39 +0100

    Tweak

--- src/pages/Ropemakers.jsx
+++ src/pages/Ropemakers.jsx
@@ -1,5 +1,6 @@
 export function Ropemakers() {
     return <>
     <img src="https://uploads1.wikiart.org/images/joaquin-sorolla/ropemakers-1893.jpg" width="75%"/>
+    <h1>Don't leave without your map!</h1>
     </>
 }
